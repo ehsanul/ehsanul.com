544b664721bd1b3dbd16b21291faba9e011dce86
Ehsanul Hoque <ehsanul@ehsanul.com>
2011-03-01 04:16:32 -0500

    first commit

---
+++ Capfile
@@ -0,0 +1,24 @@
+load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+
+set :user, 'ehsanul'
+set :domain, 'ehsanul.com'
+
+default_run_options[:pty] = true
+
+set :repository,  "ehsanul@ehsanul.com:git/ehsanul.com.git" 
+set :deploy_to, "/home/ehsanul/ehsanul.com" 
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'deploy'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :use_sudo, false
+set :ssh_options, {:forward_agent => true}
+
+server domain, :app, :web
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt" 
+  end
+end
---
+++ ehsanul.com.rb
@@ -0,0 +1,56 @@
+require 'rubygems'
+require 'sinatra'
+require 'yaml'
+require 'rdiscount'
+require 'date'
+
+Article = Struct.new(:meta, :body, :path, :summary)
+
+ARTICLES = Dir["articles/*.md"].map do |file|
+  io = File.open(file)
+  meta, body = io.read.split(/\n{3,}/)
+  io.close
+  meta = YAML::load(meta)
+  body = RDiscount.new(body).to_html
+  path = "/" + file.split("/").last.sub(/\.[^\.]+$/, '')
+  # The summary is either explicitly specified
+  # or the first two paragraphs of the article.
+  summary = ("<p>#{meta[:summary]}</p>" if meta[:summary]) ||
+            # yeah, yeah, you can't *really* parse html with regexes, i know
+            body.scan(/(<p>(?:[^<]|<\/?[^p>]*>)*<\/p>)/im)[0..1].join
+  Article.new(meta, body, path, summary)
+end.sort do |a, b|
+  b.meta[:date] <=> a.meta[:date] # descending dates
+end.each do |article|
+  get article.path do
+    @title = article.meta[:title]
+    @date  = article.meta[:date]
+    @body  = article.body
+    erb :article
+  end
+end
+
+helpers do
+  def link(text, url)
+    "<a href='#{url}'>#{text}</a>"
+  end
+  def pretty_date(date)
+    date.strftime("%d %b %Y")
+  end
+  def more_pages?(page)
+    !ARTICLES[(page+1)*5].nil?
+  end
+end
+
+get /^\/blog(?:\/(\d*))?$/ do
+  @page = params[:captures] ? params[:captures].first.to_i : 0
+  @articles = ARTICLES[ (@page*5)..((@page*5)+4) ]
+  redirect "/blog" if @articles.nil? && @page != 0 # avoid infinite loop
+  erb :blog
+end
+
+get '/' do erb :home end
+
+[:about, :projects].each do |route|
+  get "/#{route}" do erb route end
+end
